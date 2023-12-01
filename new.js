69922093e637915d15481429126ae0854d8486fe
mohsenshahii <mohsen1370shahi@gmail.com>
2023-12-01 13:09:15 +0100

    COUNTER

--- new.js
+++ new.js
@@ -1,1 +1,19 @@
-console.log("Hi");
+Given an integer n, return a counter function. This counter function initially returns n
+and then returns 1 more than the previous value every subsequent time it is called (n, n + 1, n + 2, etc).
+
+
+SOLUTION:
+
+function createCounter(n, operations) {
+    const results = [];
+    let current = n;
+
+    for (const operation of operations) {
+        if (operation === "call") {
+            results.push(current);
+            current++;
+        }
+    }
+
+    return results;
+}
